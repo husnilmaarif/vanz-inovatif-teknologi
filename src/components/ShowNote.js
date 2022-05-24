1159963021f1c6a0983900c3659dc8842c291f3d
Husnil Maarif <husnilmaarif.hm@gmail.com>
2022-05-24 09:45:31 +0700

    redux masih proses

---
+++ src/components/ShowNote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ShowNote() {
+  return (
+    <>
+      <div className="col-md-5 text-center p-5 shadow bg-light">
+        <h4>husnil maarif</h4>
+        <hr />
+        <p>bladf;alksdfjapoeduflkdjfa;lkdjf;alkdjfa;ldkfja;ldkffjaldkffj</p>
+      </div>
+    </>
+  );
+}
+
+export default ShowNote;
--- src/pages/PostingPage.js
+++ src/pages/PostingPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AddNote from "../components/AddNote";
+import ShowNote from "../components/ShowNote";
 
 function PostingPage() {
   return (
@@ -10,8 +11,12 @@ function PostingPage() {
         </div>
 
         <div className="container">
-          <div className="row justify-content-center mt-5">
+          <div className="row justify-content-center my-5">
             <AddNote />
+          </div>
+
+          <div className="row justify-content-center mt-3 mb-5">
+            <ShowNote />
           </div>
         </div>
       </div>
